a3a263e0f1cf31c2a0c2b10df6a73ddcda4835c7
MJohnsonFL <MJohnsonFL@gmail.com>
2014-08-17 09:01:33 -0400

    Added Solutions

---
+++ Easy Level/Multiples of a Number/MultiplesOfANumber.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace MultiplesOfANumber
+{
+    class MultiplesOfANumber
+    {
+        /// Program written by Michael P. Johnson
+        /// for the Multiples of a Number Challenge found on
+        /// https://www.codeeval.com/open_challenges/18/
+        /// </summary>
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(args[0]))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+                        Console.WriteLine(SmallestMultiple(Int32.Parse(line[0]), Int32.Parse(line[1])));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read: ");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static int SmallestMultiple(int x, int n)
+        {
+            int nOriginal = n;
+            while (n <= x)
+            {
+                n += nOriginal;
+            }
+            return n;
+        }
+    }
+}
---
+++ Easy Level/Reverse Words/ReverseWords.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace ReverseWords
+{
+    /// <summary>
+    /// Program written by Michael P. Johnson
+    /// for the Reverse Words Challenge found on
+    /// https://www.codeeval.com/open_challenges/8/
+    /// </summary>
+    class ReverseWords
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(args[0]))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string sentence = sr.ReadLine();
+                        if (!string.IsNullOrEmpty(sentence))
+                        {
+                            Console.WriteLine(ReverseSentence(sentence));
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read: ");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static string ReverseSentence(string sentence)
+        {
+            string[] words = sentence.Split(' ');
+            Array.Reverse(words);
+            return string.Join(" ", words).Trim();
+        }
+    }
+}
