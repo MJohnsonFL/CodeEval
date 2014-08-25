d93ef080eac7c54c9dd86c375762972ecc2bbe33
MJohnsonFL <MJohnsonFL@gmail.com>
2014-08-25 06:03:09 -0400

    Added Solutions

---
+++ Easy Level/SumOfIntegersFromFile/SumOfIntegersFromFile.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace SumOfIntegersFromFile
+{
+    /// <summary>
+    /// Program written by Michael P. Johnson
+    /// for the Sum of Integers from File Challenge found on
+    /// https://www.codeeval.com/open_challenges/24/
+    /// </summary>
+    class SumOfIntegersFromFile
+    {
+        static void Main(string[] args)
+        {
+            int Sum = 0;
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(args[0]))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        int line = Int32.Parse(sr.ReadLine());
+                        Sum += line;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read: ");
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine(Sum);
+        }
+    }
+}
---
+++ Easy Level/UniqueElements/UniqueElements.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace UniqueElements
+{
+
+
+    /// <summary>
+    /// Program written by Michael P. Johnson
+    /// for the Unique Elements Challenge found on
+    /// https://www.codeeval.com/open_challenges/29/
+    /// </summary>
+    class UniqueElements
+    {
+        public static string RemoveDupesAndSort(string[] line)
+        {
+            List<int> intList = new List<int>();
+
+            foreach (string s in line)
+            {
+                int i = Int32.Parse(s);
+                if (!intList.Contains(i))
+                    intList.Add(i);
+            }
+
+            intList.Sort();
+
+            return string.Join(",", intList);
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(args[0]))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+                        Console.WriteLine(RemoveDupesAndSort(line));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read: ");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
