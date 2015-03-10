bb96b575644c73622c01b4f9029d77ec87ac58a9
MJohnsonFL <MJohnsonFL@gmail.com>
2015-03-09 21:25:14 -0400

    Old solutions never added

---
+++ Easy Level/Multiplication Tables/MultiplicationTables.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace MultiplicationTables
+{
+    /// <summary>
+    /// Program written by Michael P. Johnson
+    /// for the Multiplication Tables Challenge found on
+    /// https://www.codeeval.com/open_challenges/23/
+    /// </summary>
+    class MultiplicationTables
+    {
+        static void Main(string[] args)
+        {
+            for (int factorOne = 1; factorOne <= 12; factorOne++)
+            {
+                StringBuilder line = new StringBuilder();
+
+                line.Append(factorOne);
+
+                for (int factorTwo = 2; factorTwo <= 12; factorTwo++)
+                {
+                    line.Append(string.Format("{0,4:####}", factorOne * factorTwo));
+                }
+                
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
---
+++ Moderate Level/Number Of Ones/NumberOfOnes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace NumberOfOnes
+{
+    /// <summary>
+    /// Program written by Michael P. Johnson
+    /// for the Number of Ones Challenge found on
+    /// https://www.codeeval.com/open_challenges/16/
+    /// </summary>
+    class NumberOfOnes
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader(args[0]))
+            {
+                while (!sr.EndOfStream)
+                {
+                    int number = Int32.Parse(sr.ReadLine());
+
+                    Console.WriteLine(CountOfOnesInBinary(number));
+                }
+            }
+        }
+
+        private static int CountOfOnesInBinary(int numberToConvert)
+        {
+            string binaryString = Convert.ToString(numberToConvert, 2);
+
+            int count = 0;
+
+            foreach (char bit in binaryString)
+            {
+                if (bit == '1')
+                    count += 1;
+            }
+
+            return count;
+        }
+    }
+}
